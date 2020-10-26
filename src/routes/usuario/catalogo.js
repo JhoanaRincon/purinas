def3a479cf59f77c8f53a61c1c60e63814f6a970
IVONNE LISETH DIAZ GELVEZ <ivonnelisethdg@ufps.edu.co>
2020-10-25 20:11:45 -0500

    ultima modificacion

--- src/routes/usuario/catalogo.js
+++ src/routes/usuario/catalogo.js
@@ -1,9 +1,10 @@
+//importa variables y conexion a base de datos 
 const express = require('express');
 const router = express.Router();
 const pool = require("../../database");
 const passport = require('passport');
 
-
+//accede a la base de datos mostrando los productos en ella 
 router.get('/catalogo',async (req, res) => {
     const productos= await pool.query("SELECT * FROM producto");
     console.log(productos);
