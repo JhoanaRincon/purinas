5729f6e1837f047fd3115d92673e38817f0f4899
JhoanaRincon <70903986+JhoanaRincon@users.noreply.github.com>
2020-10-25 23:14:37 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
         host: 'us-cdbr-east-02.cleardb.com',
-        user: 'b6a681aa4a944c',
+        user: 'b253fa87861c17',
         password: '08f8bb6e',
-        database: 'heroku_8a441495b695be5'
+        database: '621e6018'
     }
 }
