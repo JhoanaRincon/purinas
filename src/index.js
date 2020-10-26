d5c7a3c9524d01ec8c9fa82c88a26efe9a4e8aa5
JhoanaRincon <70903986+JhoanaRincon@users.noreply.github.com>
2020-10-25 22:43:46 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.engine('.hbs', exphbs({
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs',
-    helpers: require('./lib/handlebars')
+    helpers: require('./lib/helpers')
     
 }))
 app.set('view engine', '.hbs');
